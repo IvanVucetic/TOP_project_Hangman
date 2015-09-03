6e268b9fa16747f3360c93b0886af90a8825f9cc
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-03 14:57:39 +0200

    add while loop and break conditions

--- hangman.py
+++ hangman.py
@@ -19,6 +19,43 @@ print word, len(word)
 # preparing the game
 
 correct_letters = "_" * len(word)   # to display correctly guessed letters
-empty_slots = len(word) # unguessed letters; game ends when == 0 (all guessed)
-incorrect_letters = []  # display incorrect guesses to avoid repeating them
-attempts_left = 6   # TODO: turn into a gallows
+empty_slots = len(word)  # unguessed letters; game ends when == 0 (all guessed)
+incorrectly_guessed = []  # display incorrect guesses to avoid repeating them
+correctly_guessed = []  # take note of previous guesses
+attempts_left = 6   # TODO: turn into gallows
+
+while True:
+    
+    print correct_letters
+    print "Incorrect attempts: ", incorrectly_guessed
+    print "Attempts left: ", attempts_left
+
+    print "Guess new letter:"
+    new_attempt = raw_input("> ")
+    print "new attempt: ", new_attempt
+
+    if new_attempt in correctly_guessed:
+        print "You have guessed this letter already, try another."
+
+    elif new_attempt in incorrectly_guessed:
+        print "You have already tried this one."
+
+    else:
+        if new_attempt in word:
+            print "Bravo!"
+            correctly_guessed.append(new_attempt)
+            # change correct_letters to show position of guessed letter
+            # empty_slots = empty_slots - num of letter occurences in a word
+            if empty_slots == 0: 
+                print "---"
+                print "You have WON!!!"
+                break
+        
+        else:
+            print "Wrong...!"
+            incorrectly_guessed.append(new_attempt)
+            attempts_left -= 1
+            if attempts_left == 0: 
+                print "---"
+                print "You have lost... :("
+                break
